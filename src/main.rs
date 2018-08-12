bc65272de185cc764839f0b42b5760a923d9205f
John Kalyan <john.r.kalyan@gmail.com>
2018-08-12 13:02:00 -0400

    begin turret

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 extern crate ggez;
+extern crate rand;
+use rand::{thread_rng, Rng};
+
 use ggez::audio;
 use ggez::conf;
 use ggez::event::{self, EventHandler, Keycode, Mod};
@@ -8,9 +11,11 @@ use ggez::nalgebra as na;
 use ggez::timer;
 use ggez::{Context, ContextBuilder, GameResult};
 
-const WINDOW_WIDTH: f32 = 1000.0;
-const WINDOW_HEIGHT: f32 = 600.0;
+const WINDOW_WIDTH: f32 = 1200.0;
+const WINDOW_HEIGHT: f32 = 800.0;
 const UI_WIDTH: f32 = 200.0;
+
+const SHRINK: f32 = 20.0;
 
 const PLAYER_HEALTH: f32 = 1.0;
 const PLAYER_RADIUS: f32 = 8.0;
@@ -29,10 +34,24 @@ const BACK_COLOR: Color = Color {
     a: 1.0,
 };
 
+const COIN_COLOR: Color = Color {
+    r: 1.0,
+    g: 0.9,
+    b: 0.17,
+    a: 1.0,
+};
+
+const ENEMY_COLOR: Color = Color {
+    r: 1.0,
+    g: 0.38,
+    b: 0.227,
+    a: 1.0,
+};
+
 struct Object {
     pub pos: Point2,
     pub vel: Vector2,
-    pub health: f32,
+    pub alive: bool,
     pub radius: f32,
     pub speed: f32,
 }
@@ -43,12 +62,25 @@ impl Object {
     }
 }
 
+/*
+ * IDEA: give the player a gun and if they hit the wall then it shrinks but if they hit an
+ * enemy then it kills them
+ * TODO - fix the shrinking unevenly.
+ */
+
 struct State {
     player: Object,
-    projectiles: Vec<Object>,
+    enemies: Vec<Object>,
+    shots: Vec<Object>,
     boundary: Rect,
     coin: Object,
     score: u32,
+    shrink_target: Option<f32>,
+    ammo: u32,
+    angle: Vector2,
+    dirty: bool,
+    mousex: f32,
+    mousey: f32,
 }
 
 impl ggez::event::EventHandler for State {
@@ -56,11 +88,30 @@ impl ggez::event::EventHandler for State {
         // TODO restrict the frame rate
         let seconds = 0.1;
         self.move_player(seconds);
+        self.update_angle();
+        // TODO move each ball
+        for ball in &mut self.enemies {
+            move_ball(ball, seconds, self.boundary);
+        }
 
-        // TODO handle movements
-        // TODO handle collisions
-        // TODO handle 
-        // TODO handle UI changes
+        if self.player.collides_with(&self.coin) {
+            self.score += 1;
+            self.spawn_enemy();
+            self.coin.pos = self.random_location(self.coin.radius);
+            clamp_object(&mut self.coin, self.boundary);
+        }
+        for enemy in &mut self.enemies {
+            if enemy.collides_with(&self.player) {
+                self.shrink_target = match self.shrink_target {
+                    Some(t) => Some(t + SHRINK),
+                    None => Some(SHRINK)
+                };
+                enemy.alive = false;
+            }
+        }
+        self.shrink_boundary();
+        self.enemies.retain(|e| {e.alive});
+        self.shots.retain(|s| {s.alive});
         Ok(())
     }
 
@@ -73,13 +124,12 @@ impl ggez::event::EventHandler for State {
             graphics::DrawMode::Fill,
             self.boundary);
 
-        graphics::set_color(ctx, MAIN_COLOR);
-        graphics::circle(
-            ctx,
-            graphics::DrawMode::Fill,
-            self.player.pos,
-            PLAYER_RADIUS,
-            0.1);
+        draw_obj(&self.player, ctx, MAIN_COLOR);
+        self.draw_turret(ctx);
+        draw_obj(&self.coin, ctx, COIN_COLOR);
+        for enemy in &self.enemies {
+            draw_obj(enemy, ctx, ENEMY_COLOR);
+        }
         graphics::present(ctx);
         std::thread::yield_now();
         Ok(())
@@ -120,8 +170,7 @@ impl ggez::event::EventHandler for State {
             Keycode::W => {
                 self.player.vel.y += 1.0;
             }
-            Keycode::A => {
-                self.player.vel.x += 1.0;
+            Keycode::A => { self.player.vel.x += 1.0;
             }
             Keycode::S => {
                 self.player.vel.y -= 1.0;
@@ -133,6 +182,18 @@ impl ggez::event::EventHandler for State {
 
         }
     }
+    fn mouse_motion_event(&mut self,
+                          _ctx: &mut Context,
+                          _state: ggez::event::MouseState,
+                          x: i32,
+                          y: i32,
+                          _xrel: i32,
+                          _yrel: i32) {
+        self.mousex = x as f32;
+        self.mousey = y as f32;
+    }
+
+
 }
 
 fn main() {
@@ -148,22 +209,35 @@ fn main() {
             min_height: 0,
             max_height: 0,
         },
+        window_setup: conf::WindowSetup {
+            title: "Not another snake variant!".to_owned(),
+            icon: "".to_owned(),
+            resizable: false,
+            allow_highdpi: true,
+            samples: conf::NumSamples::One,
+
+        },
         ..Default::default()
     };
-    let ctx = &mut Context::load_from_conf("Inventorium", "John Kalyan", c).unwrap();
+    let ctx = &mut Context::load_from_conf("LD42", "John Kalyan", c).unwrap();
     graphics::set_background_color(ctx, MAIN_COLOR);
 
     let padding = 10.0;
     let state = &mut State { 
         player: create_player(),
-        projectiles: Vec::new(),
+        enemies: Vec::new(),
+        shots: Vec::new(),
         boundary: Rect::new(UI_WIDTH + padding,
-                            0.0 + padding,
-                            WINDOW_WIDTH - UI_WIDTH - 2.0 * padding,
+                            0.0 + padding, WINDOW_WIDTH - UI_WIDTH - 2.0 * padding,
                             WINDOW_HEIGHT - 2.0 * padding),
         score: 0,
-        coin: create_coin()
-
+        coin: create_coin(),
+        shrink_target: None,
+        ammo: 0,
+        angle: Vector2::new(1.0, 0.0),
+        dirty: true,
+        mousex: 0.0,
+        mousey: 0.0
     };
 
     event::run(ctx, state).unwrap();
@@ -173,9 +247,9 @@ fn main() {
 fn create_player() -> Object {
     // TODO spawn the player in a better spot
     Object {
-        pos: Point2::new(WINDOW_WIDTH + UI_WIDTH + 30.0, WINDOW_HEIGHT / 2.0),
+        pos: Point2::new(UI_WIDTH + 30.0, WINDOW_HEIGHT / 2.0),
         vel: Vector2::new(0.0, 0.0),
-        health: PLAYER_HEALTH,
+        alive: true,
         radius: PLAYER_RADIUS,
         speed: 100.0,
     }
@@ -185,77 +259,147 @@ fn create_coin() -> Object {
     Object {
         pos: Point2::new(WINDOW_WIDTH - 30.0, WINDOW_HEIGHT / 2.0),
         vel: Vector2::new(0.0, 0.0),
-        health: 100.0, // redundant
-        radius: 16.0,
+        alive: true,
+        radius: 6.0,
         speed: 0.0,
     }
-}
-
-// TODO - create enemies according to level
-fn create_enemies() -> Vec<Object> {
-    Vec::new()
 }
 
 // TODO extract the player function
 impl State {
     fn move_player(&mut self, dt: f32) {
-        let left = self.boundary.x + self.player.radius;
-        let right = self.boundary.x + self.boundary.w - self.player.radius;
-        let top  = self.boundary.y + PLAYER_RADIUS;
-        let bot = self.boundary.y + self.boundary.h - self.player.radius;
         move_object(&mut self.player, dt);
-        if self.player.pos.x > right {
-            self.player.pos.x = right;
-        }
-        if self.player.pos.y > bot {
-            self.player.pos.y = bot;
-        }
-        if self.player.pos.x < left {
-            self.player.pos.x = left;
-        }
-        if self.player.pos.y < top {
-            self.player.pos.y = top;
-        }
-    }
-
-    fn move_balls(&mut self, ball: &mut Object, dt: f32) {
-        let left = self.boundary.x + ball.radius;
-        let right = self.boundary.x + self.boundary.w - ball.radius;
-        let top  = self.boundary.y + PLAYER_RADIUS;
-        let bot = self.boundary.y + self.boundary.h - ball.radius;
-        move_object(ball, dt);
-        if ball.pos.x > right {
-            ball.pos.x = right;
-            ball.vel.x = -ball.vel.x;
-        }
-        if ball.pos.y > bot {
-            ball.pos.y = bot;
-            ball.vel.y = -ball.vel.y;
-        }
-        if ball.pos.x < left {
-            ball.pos.x = left;
-            ball.vel.x = -ball.vel.x;
-        }
-        if ball.pos.y < top {
-            ball.pos.y = top;
-            ball.vel.y = -ball.vel.y;
-        }
+        clamp_object(&mut self.player, self.boundary);
     }
 
     fn shrink_boundary(&mut self) {
-        let shrinkage = 10.0;
-        self.boundary.x += shrinkage;
-        self.boundary.y += shrinkage / 2.0;
-        self.boundary.w -= 2.0 * shrinkage;
-        self.boundary.h -= shrinkage;
+        if let Some(t) = self.shrink_target {
+            if t > 0.0 {
+                let shrinkage = 1.0;
+                self.boundary.x += shrinkage;
+                self.boundary.y += shrinkage;
+                self.boundary.w -= 2.0 * shrinkage;
+                self.boundary.h -= 2.0 * shrinkage;
+                self.shrink_target = Some(t - shrinkage);
+                clamp_object(&mut self.coin, self.boundary);
+            }
+        }
     }
 
-    fn random_location(&self) -> Point2 {
-        // TODO
-        //if self.player.pos < self.boundary.x + self.boundary.w / 2.0 {
-        //}
-        Point2::new(50.0,  50.0)
+    fn random_location(&self, rad: f32) -> Point2 {
+        let mut rng = thread_rng();
+        let mut startx = self.boundary.x + rad;
+        if self.player.pos.x < self.boundary.x + self.boundary.w / 2.0 {
+            startx += self.boundary.w / 2.0;
+        }
+        let randx: f32 = rng.gen_range(startx, startx + self.boundary.w / 2.0);
+        let starty: f32 = self.boundary.y + rad;
+        let randy = rng.gen_range(starty, starty + self.boundary.h);
+        Point2::new(randx,  randy)
     }
+
+    fn spawn_enemy(&mut self) {
+        let mut rng = thread_rng();
+        let radius = 8.0;
+        let velx = rng.gen_range(0.2, 0.5);
+        let vely = rng.gen_range(0.2, 0.5);
+        let vel = Vector2::new(velx, vely);
+        let enemy = Object {
+            pos: self.random_location(radius),
+            alive: true,
+            radius: radius,
+            speed: rng.gen_range(100.0,200.0),
+            vel: vel,
+        };
+        self.enemies.push(enemy);
+    }
+
+    fn draw_turret(&self, ctx: &mut Context) {
+        graphics::set_color(ctx, MAIN_COLOR);
+        let point = self.angle * 20.0;
+        let mut point =  Point2::new(point.x, point.y);
+        point.x += self.player.pos.x;
+        point.y += self.player.pos.y;
+        graphics::circle(
+            ctx,
+            graphics::DrawMode::Fill,
+            Point2::new(point.x, point.y),
+            4.0,
+            0.01).unwrap();
+    }
+
+    fn update_angle(&mut self) {
+        let diff = Vector2::new(self.mousex, self.mousey) - 
+            Vector2::new(self.player.pos.x, self.player.pos.y);
+        self.angle = diff.normalize();
+    }
+
+    fn fire_shot(&mut self) {
+        let spawn = self.angle * 20.0;
+        let spawn = Point2::new(spawn.x + self.player.pos.x, spawn.y + self.player.pos.y);
+        let shot = Object {
+            radius: 3.0,
+            vel: self.angle,
+            speed: 200.0,
+            alive: true,
+            pos: spawn
+        };
+        self.shots.push(shot);
+    }
+}
+
+fn move_ball(ball: &mut Object, dt: f32, boundary: Rect) {
+    let left = boundary.x + ball.radius;
+    let right = boundary.x + boundary.w - ball.radius;
+    let top  = boundary.y + ball.radius;
+    let bot = boundary.y + boundary.h - ball.radius;
+    move_object(ball, dt);
+    if ball.pos.x > right {
+        ball.pos.x = right;
+        ball.vel.x = -ball.vel.x;
+    }
+    if ball.pos.y > bot {
+        ball.pos.y = bot;
+        ball.vel.y = -ball.vel.y;
+    }
+    if ball.pos.x < left {
+        ball.pos.x = left;
+        ball.vel.x = -ball.vel.x;
+    }
+    if ball.pos.y < top {
+        ball.pos.y = top;
+        ball.vel.y = -ball.vel.y;
+    }
+}
+
+
+fn draw_obj(obj: &Object, ctx: &mut Context, color: Color) {
+    graphics::set_color(ctx, color);
+    graphics::circle(
+        ctx,
+        graphics::DrawMode::Fill,
+        obj.pos,
+        obj.radius,
+        0.01);
+}
+
+fn clamp_object(obj: &mut Object, boundary: Rect) {
+        let left = boundary.x + obj.radius;
+        let right = boundary.x + boundary.w - obj.radius;
+        let top  = boundary.y + PLAYER_RADIUS;
+        let bot = boundary.y + boundary.h - obj.radius;
+        if obj.pos.x > right {
+            obj.pos.x = right;
+        }
+        if obj.pos.y > bot {
+            obj.pos.y = bot;
+        }
+        if obj.pos.x < left {
+            obj.pos.x = left;
+        }
+        if obj.pos.y < top {
+            obj.pos.y = top;
+        }
 }
 
 fn move_object(obj: &mut Object, dt: f32) {
